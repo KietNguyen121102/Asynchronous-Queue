6098213350809c8d8907b97d9ab2e8aa5374b983
KietNguyen121102 <knguy38@u.rochester.edu>
2023-11-15 18:24:32 -0500

    --exportdata

--- AvgCostCalculation.java
+++ AvgCostCalculation.java
@@ -1,30 +1,36 @@
+import java.util.*;
+
 public class AvgCostCalculation {
 // This is for an even split of baristas
-    public static double calculatingQueueCost(int queueSize, int lineSize, double numBarista, double alpha, double processingTime){
+    public static double calculatingQueueCost(int queueSize, int lineSize, double queueBarista, double lineBarista, double alpha, double processingTime){
 
         double queueWaitingTime = 0;
         double totalCustomer = queueSize + lineSize;
        
 
-        if(lineSize > queueSize){
-            queueWaitingTime = (float) processingTime*alpha*((queueSize+1)/numBarista);
+        if(lineSize/lineBarista >= queueSize/queueBarista){
+            queueWaitingTime = (float) (Math.ceil((queueSize + 1)/queueBarista)*processingTime)*alpha;
         } 
         else{
-            queueWaitingTime = (float) processingTime*alpha*(1/(numBarista))*(2*(totalCustomer)-queueSize-(totalCustomer*(totalCustomer-1))/(2*queueSize));
+            if(((queueSize - queueBarista*Math.floor(lineSize/lineBarista)+1) % (queueBarista + lineBarista)) > ((lineBarista + queueBarista) - (lineSize % lineBarista)))
+            {queueWaitingTime = (float) alpha*(Math.floor(lineSize/lineBarista)*processingTime + Math.ceil((queueSize - queueBarista*Math.floor(lineSize/lineBarista) + 1 )/(queueBarista + lineBarista))*processingTime + processingTime);}
+            else{queueWaitingTime = (float) alpha*(Math.floor(lineSize/lineBarista)*processingTime + Math.ceil((queueSize - queueBarista*Math.floor(lineSize/lineBarista) + 1 )/(queueBarista + lineBarista))*processingTime);}
         }
 
         return queueWaitingTime;
     }
 
-    public static double calculatingLineCost(int queueSize, int lineSize, double numBarista, double alpha, double processingTime){
+    public static double calculatingLineCost(int queueSize, int lineSize, double queueBarista, double lineBarista, double alpha, double processingTime){
         double lineWaitingTime = 0;
         double totalCustomer = queueSize + lineSize;
 
-        if(lineSize > queueSize){
-            lineWaitingTime = (float) processingTime*(1/(numBarista))*(2*(totalCustomer)-lineSize-(totalCustomer*(totalCustomer-1))/(2*lineSize));      
+        if(lineSize/lineBarista >= queueSize/queueBarista){
+            if(((lineSize - lineBarista*Math.floor(queueSize/queueBarista)+1) % (queueBarista + lineBarista)) > ((lineBarista + queueBarista) - (queueSize % queueBarista)))
+            {lineWaitingTime = (float) (Math.floor(queueSize/queueBarista)*processingTime + Math.ceil((lineSize - lineBarista*Math.floor(queueSize/queueBarista) + 1 )/(queueBarista + lineBarista))*processingTime + processingTime);}
+            else{lineWaitingTime = (float) (Math.floor(queueSize/queueBarista)*processingTime + Math.ceil((lineSize - lineBarista*Math.floor(queueSize/queueBarista) + 1 )/(queueBarista + lineBarista))*processingTime);}     
         }
         else{
-            lineWaitingTime = (float) processingTime*(lineSize+1)/numBarista;
+            lineWaitingTime = (float) processingTime*Math.ceil((lineSize+1)/lineBarista);
         }
 
         return lineWaitingTime;
